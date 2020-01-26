1b7f718712a845b9fd13f77047f887518906758d
Michał Kuwik <Michal7074@poczta.fm>
2020-01-26 21:04:31 +0100

    add css.modules to project,create component FetchSeries and add input type text inside it

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import styles from './app.module.css';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className={styles.app}>
+      <header className={styles.app_header}>
+        <h1 className={styles.app_title}>
+          Tv series list
+        </h1>
+      </header>
+     {/* <Main /> */}
     </div>
   );
 }
---
+++ src/components/FetchSeries/FetchSeries.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function FetchSeries(){
+
+
+  return(
+    <div>
+      <div>
+        <input 
+          type="text" 
+          onChange={}
+          value={}
+          />
+      </div>
+
+
+    </div>
+  )
+}
+
+export default FetchSeries;
