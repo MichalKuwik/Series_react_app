30789544d23e82186c52f3568d04c6d5dbfd1970
Michał Kuwik <Michal7074@poczta.fm>
2020-01-28 19:03:05 +0100

    add useState hooks, add input change chandler and fetch data from input value

--- src/components/FetchSeries/FetchSeries.js
+++ src/components/FetchSeries/FetchSeries.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+
+const base_url = 'http://api.tvmaze.com'; 
 
 function FetchSeries(){
+
+  const [series,setSeries] = useState([]);
+  const [seriesName,setSeriesName] = useState('');
+  const [isFetching,setIsFetching] = useState(false)
+
+  const chandleInputChange = (e) => {
+
+    setSeriesName(e.target.value);
+    setIsFetching(true);
+
+    fetch(`${base_url}/search/shows?q=${e.target.value}`)
+    .then(res => res.json())
+    .then(data => {
+      setSeries(data)
+      setIsFetching(false)
+    })
+    .then(err => console.log(err))
+  }
 
 
   return(
@@ -8,8 +28,8 @@ function FetchSeries(){
       <div>
         <input 
           type="text" 
-          onChange={}
-          value={}
+          onChange={chandleInputChange}
+          value={series}
           />
       </div>
 
