7d05d76a1103c57b312bbe64ff44f55120c2817f
Michał Kuwik <Michal7074@poczta.fm>
2020-03-22 20:42:40 +0100

    create GlobalStyle for global styles for entire app,and create MainWrapper using styled-components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import Main from '../Main/Main';
-import styles from './app.module.css';
 import ScrollUpButton from 'react-scroll-up-button';
+import styled from 'styled-components';
 
-
+const MainWrapper = styled.div`
+  text-align: center;
+  margin-top:2rem;
+`
 
 function App() {
-
   return (
-    <div className={styles.app}>
+    <MainWrapper>
      <Main />
      <ScrollUpButton 
       style={{background:'none'}}
       ShowAtPosition={10}
       />
-    </div>
+    </MainWrapper>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App/App';
 import {BrowserRouter} from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 
+
+const GlobalStyle = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,900&display=swap');
+
+  body {
+    margin: 0;
+    background:#000;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 300;
+  }
+`
 
 ReactDOM.render(
 <BrowserRouter>
+  <GlobalStyle />
   <App />
 </BrowserRouter>
 , document.getElementById('root'));
