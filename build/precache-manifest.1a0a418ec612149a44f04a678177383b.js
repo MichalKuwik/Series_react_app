e218488e833c8e72cdcc5c84174a312ae01b1f4c
Michał Kuwik <Michal7074@poczta.fm>
2020-04-10 20:21:59 +0200

    build version to production and deploy

---
+++ build/precache-manifest.1a0a418ec612149a44f04a678177383b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3f8d3f0ad5c87b8910cf33fe31d8df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38c4e7ac0e5c8d5ac59c",
+    "url": "/static/js/2.3cdce0b4.chunk.js"
+  },
+  {
+    "revision": "9155c08adaa4e0d8a8d51799f5eb4bbb",
+    "url": "/static/js/2.3cdce0b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4991d3a7d3cea88539e",
+    "url": "/static/js/main.99ff573c.chunk.js"
+  },
+  {
+    "revision": "09536763b1c43e681405",
+    "url": "/static/js/runtime-main.79795858.js"
+  },
+  {
+    "revision": "e374cd5cf5f4680bdbaa1bc1ebccfce7",
+    "url": "/static/media/icon.e374cd5c.png"
+  },
+  {
+    "revision": "6e29f5a8ddf2b732ee98b54301838630",
+    "url": "/static/media/loader.6e29f5a8.gif"
+  },
+  {
+    "revision": "41655e840da25695a095df2d0eb432a0",
+    "url": "/static/media/move-logo.41655e84.png"
+  },
+  {
+    "revision": "cc1f1c45ae3db38ef75db214c0a66926",
+    "url": "/static/media/rating.cc1f1c45.png"
+  },
+  {
+    "revision": "0e5c26eaf039da90220adb0c18b036c1",
+    "url": "/static/media/returnArrow.0e5c26ea.png"
+  },
+  {
+    "revision": "d78f0ed6a98ad9e81eaa8406d8a9f3ce",
+    "url": "/static/media/sad.d78f0ed6.png"
+  },
+  {
+    "revision": "1c2bd4cdc18f2f41d35a699d6d0d1b86",
+    "url": "/static/media/tape.1c2bd4cd.png"
+  },
+  {
+    "revision": "7746e4c474455664b06efd5573485902",
+    "url": "/static/media/translating.7746e4c4.png"
+  }
+]);
