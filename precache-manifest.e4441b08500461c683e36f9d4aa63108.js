1c30f4ffb04f7ee9981d917427a95e4bfe53685f
Michał Kuwik <Michal7074@poczta.fm>
2020-04-10 21:50:06 +0200

    Updates

---
+++ precache-manifest.e4441b08500461c683e36f9d4aa63108.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4526806fa6f808eafbc57e4cf44355f1",
+    "url": "/Series_react_app/index.html"
+  },
+  {
+    "revision": "93ff501fc3651d129856",
+    "url": "/Series_react_app/static/js/2.2872a428.chunk.js"
+  },
+  {
+    "revision": "9155c08adaa4e0d8a8d51799f5eb4bbb",
+    "url": "/Series_react_app/static/js/2.2872a428.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4991d3a7d3cea88539e",
+    "url": "/Series_react_app/static/js/main.99ff573c.chunk.js"
+  },
+  {
+    "revision": "345664567f7f39eb69e2",
+    "url": "/Series_react_app/static/js/runtime-main.e2f87d0b.js"
+  },
+  {
+    "revision": "e374cd5cf5f4680bdbaa1bc1ebccfce7",
+    "url": "/Series_react_app/static/media/icon.e374cd5c.png"
+  },
+  {
+    "revision": "6e29f5a8ddf2b732ee98b54301838630",
+    "url": "/Series_react_app/static/media/loader.6e29f5a8.gif"
+  },
+  {
+    "revision": "41655e840da25695a095df2d0eb432a0",
+    "url": "/Series_react_app/static/media/move-logo.41655e84.png"
+  },
+  {
+    "revision": "cc1f1c45ae3db38ef75db214c0a66926",
+    "url": "/Series_react_app/static/media/rating.cc1f1c45.png"
+  },
+  {
+    "revision": "0e5c26eaf039da90220adb0c18b036c1",
+    "url": "/Series_react_app/static/media/returnArrow.0e5c26ea.png"
+  },
+  {
+    "revision": "d78f0ed6a98ad9e81eaa8406d8a9f3ce",
+    "url": "/Series_react_app/static/media/sad.d78f0ed6.png"
+  },
+  {
+    "revision": "1c2bd4cdc18f2f41d35a699d6d0d1b86",
+    "url": "/Series_react_app/static/media/tape.1c2bd4cd.png"
+  },
+  {
+    "revision": "7746e4c474455664b06efd5573485902",
+    "url": "/Series_react_app/static/media/translating.7746e4c4.png"
+  }
+]);
